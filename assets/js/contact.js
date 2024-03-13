e54a2ce10289e0bfa1b106d14f849e3e5ef964fc
denkikumo <132528953+denkikumo@users.noreply.github.com>
2024-03-13 19:34:34 -0400

    Testcmd

---
+++ assets/js/contact.js
@@ -0,0 +1,30 @@
+// Sidebar Menu //
+
+document.querySelector('.hamburger').addEventListener('click', function(event) {
+  const sidebar = document.querySelector('.sidebar');
+  const overlay = document.querySelector('.sidebar-overlay');
+  // Toggle sidebar visibility
+  if (sidebar.style.left === '0px') {
+      sidebar.style.left = '-280px';
+      overlay.style.display = 'none'; // Hide overlay
+  } else {
+      sidebar.style.left = '0px';
+      overlay.style.display = 'block'; // Show overlay
+  }
+  event.stopPropagation();
+  });
+  
+  document.addEventListener('click', function(event) {
+  const sidebar = document.querySelector('.sidebar');
+  const overlay = document.querySelector('.sidebar-overlay');
+  if (sidebar.style.left === '0px' && !event.target.closest('.sidebar') && !event.target.classList.contains('hamburger')) {
+      sidebar.style.left = '-280px';
+      overlay.style.display = 'none'; // Hide overlay
+  }
+  });
+  
+  document.querySelector('.sidebar-overlay').addEventListener('click', function() {
+  // Close the sidebar when the overlay is clicked
+  document.querySelector('.sidebar').style.left = '-280px';
+  this.style.display = 'none'; // Hide overlay
+  });
